8d40800f7629268f33b45dc16cb1bc1b1b0a5053
wildevsum <williamdevonsumitro@gmail.com>
2025-12-23 17:59:20 +0700

    Refactor argument parsing and MLflow setup
    Removed experiment_name argument and adjusted MLflow run handling.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -10,11 +10,9 @@ import mlflow.sklearn
 # --- Argument parser ---
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
-parser.add_argument("--experiment_name", type=str, required=True)
 args = parser.parse_args()
 
 DATA_PATH = args.data_path
-EXPERIMENT_NAME = args.experiment_name
 
 # --- Load dataset ---
 df = pd.read_csv(DATA_PATH)
@@ -29,10 +27,9 @@ X_train, X_test, y_train, y_test = train_test_split(
 
 # --- MLflow setup ---
 mlflow.set_tracking_uri(f"file://{os.path.dirname(os.path.abspath(__file__))}/mlruns")
-mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
 # --- Train model ---
-with mlflow.start_run(run_name="training"):
+with mlflow.start_run():  # Jangan beri run_name jika ingin MLflow CLI yang handle
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
