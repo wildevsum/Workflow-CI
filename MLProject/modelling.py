aa108f499612357e7b44ebb308672b6a479765f5
wildevsum <williamdevonsumitro@gmail.com>
2025-12-23 17:54:27 +0700

    Make experiment_name argument required in parser

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,33 +7,32 @@ from sklearn.ensemble import RandomForestClassifier
 import mlflow
 import mlflow.sklearn
 
-# Argument parser
+# --- Argument parser ---
 parser = argparse.ArgumentParser()
 parser.add_argument("--data_path", type=str, required=True)
-parser.add_argument("--experiment_name", type=str, default="Fitness Experiment")  # Tambahkan parameter
+parser.add_argument("--experiment_name", type=str, required=True)
 args = parser.parse_args()
 
-# Dataset path
 DATA_PATH = args.data_path
 EXPERIMENT_NAME = args.experiment_name
 
-# Load data
+# --- Load dataset ---
 df = pd.read_csv(DATA_PATH)
 X = df.drop(columns=['is_fit'])
 y = df['is_fit']
 X = X.select_dtypes(include=[np.number])
 
-# Split data
+# --- Split data ---
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# MLflow setup
+# --- MLflow setup ---
 mlflow.set_tracking_uri(f"file://{os.path.dirname(os.path.abspath(__file__))}/mlruns")
-mlflow.set_experiment(EXPERIMENT_NAME)  # Gunakan parameter
+mlflow.set_experiment(EXPERIMENT_NAME)
 mlflow.sklearn.autolog()
 
-# Training
+# --- Train model ---
 with mlflow.start_run(run_name="training"):
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
